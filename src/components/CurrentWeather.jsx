5d84c32045359019ad2b5bc6cfffb8e0f09b52d3
gaatvul <kungfuscience@hotmail.com>
2023-03-05 22:06:21 +0000

    - Added useEffect for querying the OpenWeather API each time a user searches for a place. - Moved formatTimestamp method to the Context. - Added formatting and styling for the return JSX expression.

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,47 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import OpenWeather from '../apis/OpenWeather'
+import { useGlobalContext } from '../context/context'
 
 const CurrentWeather = () => {
 
-    const [currentWeather, setCurrentWeather] = useState()
-
-    const formatTimestamp = (UNIXTimestamp) => {
-        const formattedTimestamp = new Date(UNIXTimestamp * 1000).toUTCString()
-        return formattedTimestamp
-    }
+    const {
+        formatTimestamp, toCelsius, place, countryCodes,
+        currentWeather, setCurrentWeather, icons } = useGlobalContext()
 
     useEffect(() => {
         const getCurrentWeather = async () => {
             try {
-                const response = await OpenWeather.get("/weather", {
+                const response = await OpenWeather.get("/data/2.5/weather", {
                     params: {
-                        lat: 53.23,
-                        lon: -6.12,
+                        lat: place.lat,
+                        lon: place.lon,
                     }
                 })
-                console.log(response.data);
                 setCurrentWeather(response.data)
             } catch (error) {
                 console.error(error);
             }
-
         }
         getCurrentWeather()
-    }, [])
+    }, [place])
 
     return (
-        <div>
+        <div className='current-weather'>
             {currentWeather && (
-                <div>
-                    <p>{currentWeather.name}</p>
-                    <p>{currentWeather.weather[0].description}</p>
-                    <p>Temp: {currentWeather.main.temp}</p>
-                    <p>min: {currentWeather.main.temp_min}</p>
-                    <p>max: {currentWeather.main.temp_max}</p>
-                    <p>Time: {formatTimestamp(currentWeather.dt)}</p>
-                    <p>Sunrise: {formatTimestamp(currentWeather.sys.sunrise)}</p>
-                    <p>Sunset: {formatTimestamp(currentWeather.sys.sunset)}</p>
-                </div>
+                <>
+                    <div className='temperature'>{toCelsius(currentWeather.main.temp)} <div className='units'>℃</div> </div>
+                    <div className="time-location">
+                        <p className='location'>{currentWeather.name}, {countryCodes.get(currentWeather.sys.country)}</p>
+                        <p className='time'>{formatTimestamp(currentWeather.dt)}</p>
+                    </div>
+                    <div className="weather">
+                        <div className='weather-icon'>{icons.get(currentWeather.weather[0].main)}</div>
+                        <p className='weather-description'>{currentWeather.weather[0].description}</p>
+                    </div>
+                </>
             )}
         </div>
     )
