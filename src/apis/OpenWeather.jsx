d2d8320129db173658afa95940514b2a2e24e506
gaatvul <kungfuscience@hotmail.com>
2023-03-05 21:57:30 +0000

    Updated OpenWeather URL to allow querying of Geocoding API within Context component.

--- src/apis/OpenWeather.jsx
+++ src/apis/OpenWeather.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const key = import.meta.env.VITE_OPEN_WEATHER_API_KEY
 
 export default axios.create({
-    baseURL:"http://api.openweathermap.org/data/2.5",
+    baseURL:"http://api.openweathermap.org",
     params: {
         appid: key
     }
