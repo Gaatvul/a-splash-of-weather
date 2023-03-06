12264becf4136b3b60effc8e3aa8a6b1fe9735e0
gaatvul <kungfuscience@hotmail.com>
2023-03-06 12:36:23 +0000

    Updated OpenWeather API to make requests over HTTPS.

--- src/apis/OpenWeather.jsx
+++ src/apis/OpenWeather.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const key = import.meta.env.VITE_OPEN_WEATHER_API_KEY
 
 export default axios.create({
-    baseURL:"http://api.openweathermap.org",
+    baseURL:"https://api.openweathermap.org",
     params: {
         appid: key
     }
